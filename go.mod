0c2761d333e3cd0ef65340e40b12ae1a292ccf7f
armanokka <armanokka@mail.ru>
2021-06-03 12:21:31 +0300

    first commit

--- go.mod
+++ go.mod
@@ -1,14 +1,12 @@
-module github.com/heroku/go-getting-started
+module botranslate
 
-go 1.12
+go 1.16
 
 require (
-	github.com/gin-gonic/gin v0.0.0-20150626140855-4cc2de6207f4
-	github.com/heroku/x v0.0.0-20171004170240-705849e307dd
-	github.com/manucorporat/sse v0.0.0-20150604091100-c142f0f1baea // indirect
-	github.com/mattn/go-colorable v0.0.0-20150625154642-40e4aedc8fab // indirect
-	github.com/mattn/go-isatty v0.0.0-20150814002629-7fcbc72f853b // indirect
-	github.com/stretchr/testify v1.3.0 // indirect
-	golang.org/x/net v0.0.0-20150629084131-d9558e5c97f8 // indirect
-	gopkg.in/bluesuncorp/validator.v5 v5.9.1 // indirect
+	github.com/detectlanguage/detectlanguage-go v1.0.1
+	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
+	github.com/k0kubun/pp v3.0.1+incompatible
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	gorm.io/driver/postgres v1.1.0
+	gorm.io/gorm v1.21.10
 )
--- main.go
+++ main.go
@@ -1,29 +1,72 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
+	"github.com/detectlanguage/detectlanguage-go"
+	"github.com/k0kubun/pp"
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+	"io/ioutil"
 	"net/http"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+	"net/url"
 )
 
-func main() {
-	port := os.Getenv("PORT")
+type TranslateAPIResponse struct {
+	Align []string `json:"align"`
+	Code  int      `json:"code"`
+	Lang  string   `json:"lang"`
+	Text  []string `json:"text"`
+}
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
+func Translate(lang, text string) (TranslateAPIResponse, error) {
+	req, err := http.NewRequest("GET", "https://just-translated.p.rapidapi.com?lang=" + url.QueryEscape(lang) + "&text=How?", nil)
+	if err != nil {
+		return TranslateAPIResponse{}, err
+	}
+	req.Header["x-rapidapi-key"] = []string{"561a41f76amsha7c0323d47335aep1986ecjsn4e41c7c2b518"}
+	req.Header["x-rapidapi-host"] = []string{"just-translated.p.rapidapi.com"}
+	req.Header["useQueryString-type"] = []string{"true"}
+	var client http.Client
+	res, err := client.Do(req)
+	if err != nil {
+		return TranslateAPIResponse{}, err
+	}
+	response, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return TranslateAPIResponse{}, err
+	}
+	var out TranslateAPIResponse
+	err = json.Unmarshal(response, &out)
+	if err != nil {
+		return TranslateAPIResponse{}, err
+	}
+	return out, err
+}
+
+func DetectLanguage(text string) (string, error){
+	state := detectlanguage.New("c71fb63df8bdc8ea8bc4c0b20771aa5f")
+	detections, err := state.Detect(text)
+	if err != nil {
+		return "", err
+	}
+	return detections[0].Language, nil
+}
+
+func main() {
+	// Initializing PostgreSQL DB
+	var err error
+	db, err = gorm.Open(postgres.Open("host=ec2-54-247-158-179.eu-west-1.compute.amazonaws.com user=ixjrwpdkbmlzas password=bc6cf01fc60625a154f3398bf506891016818fa5183154abc2b2c8b1f09a2b7f dbname=dc6ad698nlfv2m port=5432 TimeZone=Europe/Moscow"), &gorm.Config{})
+	if err != nil {
+		panic(err)
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	// Initializing bot
+	const botToken string = "1757580922:AAHc0hz1sdqG216mFnqMZXTaDJA2cKuC1H0"
+	bot, err = tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		panic(err)
+	}
+	bot.Debug = false // >:(
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
 
-	router.Run(":" + port)
 }
