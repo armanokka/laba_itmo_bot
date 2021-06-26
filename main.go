be40be241ede8d3e7dc5ece87f0a57738d295ad6
armanokka <armanokka@mail.ru>
2021-06-26 21:36:33 +0300

    Химичу с инлайном

--- main.go
+++ main.go
@@ -392,7 +392,7 @@ func botRun(update *tgbotapi.Update) {
 	}
 	if update.InlineQuery != nil {
 		
-		langs := []string{"zh","es","en","hi","ar","bn","pt","ru","ja","mr","te","ms","tr","ko","fr","de","vi","ta","ur","jv","it","fa","gu","ab","aa","af","ak","sq","am","an","hy","as","av","ae","ay","az","bm","ba","eu","be","bh","bi","bs","br","bg","my","ca","ch","ce","ny","cv","kw","co","cr","hr","cs","da","dv","nl","eo","et","ee","fo","fj","fi","ff","gl","ka","el","gn","ht","ha","he","hz","ho","hu","ia","id","ie","ga","ig","ik","io","is","iu","kl","kn","kr","ks","kk","km","ki","rw","ky","kv","kg","ku","kj","la","lb","lg","li","ln","lo","lt","lu","lv","gv","mk","mg","ml","mt","mi","mh","mn","na","nv","nb","nd","ne","ng","nn","no","ii","nr","oc","oj","cu","om","or","os","pa","pi","pl","ps","qu","rm","rn","ro","sa","sc","sd","se","sm","sg","sr","gd","sn","si","sk","sl","so","st","su","sw","ss","sv","tg","th","ti","bo","tk","tl","tn","to","ts","tt","tw","ty","ug","uk","uz","ve","vo","wa","cy","wo","fy","xh","yi","yo","za"}
+		langs := []string{"en","ru","es","ja","ar","fr","de","af","uk","uz","ko","zh","hi","bn","pt","mr","te","ms","tr","vi","ta","ur","jv","it","fa","gu","ab","aa","ak","sq","am","an","hy","as","av","ae","ay","az","bm","ba","eu","be","bh","bi","bs","br","bg","my","ca","ch","ce","ny","cv","kw","co","cr","hr","cs","da","dv","nl","eo","et","ee","fo","fj","fi","ff","gl","ka","el","gn","ht","ha","he","hz","ho","hu","ia","id","ie","ga","ig","ik","io","is","iu","kl","kn","kr","ks","kk","km","ki","rw","ky","kv","kg","ku","kj","la","lb","lg","li","ln","lo","lt","lu","lv","gv","mk","mg","ml","mt","mi","mh","mn","na","nv","nb","nd","ne","ng","nn","no","ii","nr","oc","oj","cu","om","or","os","pa","pi","pl","ps","qu","rm","rn","ro","sa","sc","sd","se","sm","sg","sr","gd","sn","si","sk","sl","so","st","su","sw","ss","sv","tg","th","ti","bo","tk","tl","tn","to","ts","tt","tw","ty","ug","ve","vo","wa","cy","wo","fy","xh","yi","yo","za"}
 		
 		from, err := translate.DetectLanguageGoogle(update.InlineQuery.Query)
 		if err != nil {
@@ -401,26 +401,35 @@ func botRun(update *tgbotapi.Update) {
 		}
 		
 		var offset int
-		if update.InlineQuery.Offset != "" {
-			pp.Println(update.InlineQuery)
+		
+		if update.InlineQuery.Offset != "" { // Ищем смещение
 			offset, err = strconv.Atoi(update.InlineQuery.Offset)
 			if err != nil {
 				warn(-2, err)
 				return
 			}
-			if offset > len(langs) - 1 {
-				warn(-3, err)
-				return
-			}
 		}
+		
+		if offset >= len(langs) { // Слишком большое смещение
+			warn(-3, err)
+			return
+		}
+		
 		end := offset + 10
+
 		results := make([]interface{}, 0, 10)
 		for ;offset < end; offset++ {
+			if x := langs[offset]; x == "" {
+				break
+			}
 			to := langs[offset] // language code to translate
 			tr, err := translate.TranslateGoogle(from, to, update.InlineQuery.Query)
 			if err != nil {
 				warn(-4, err)
 				return
+			}
+			if tr.Text == "" {
+				continue // ну не вышло, так не вышло, че бубнить-то
 			}
 			inputMessageContent := map[string]interface{}{
 				"message_text":tr.Text,
@@ -436,13 +445,17 @@ func botRun(update *tgbotapi.Update) {
 				Description:         cutString(tr.Text, 40),
 			})
 		}
-
-
+		
+		var nextOffset int
+		if end < len(langs) {
+			nextOffset = end
+		}
+		
 		ok, err := bot.AnswerInlineQuery(tgbotapi.InlineConfig{
 			InlineQueryID:     update.InlineQuery.ID,
 			Results:           results,
 			CacheTime:         300,
-			NextOffset: 	   strconv.Itoa(end+1),
+			NextOffset: 	   strconv.Itoa(nextOffset),
 			IsPersonal:        false,
 			SwitchPMText:      "Translo",
 			SwitchPMParameter: "from_inline",
