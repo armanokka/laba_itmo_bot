f8bf9d16a10c4663c47d8bbe283d77300fde0aa6
armanokka <armanokka@mail.ru>
2021-07-07 13:53:44 +0300

    f

--- message.go
+++ message.go
@@ -76,6 +76,23 @@ func handleMessage(update *tgbotapi.Update) {
             return
         }
         analytics.Bot(update.Message.Chat.ID, msg.Text, "Main menu")
+    
+        
+        parts := strings.Fields(update.Message.Text)
+        if len(parts) == 2 {
+            var referrer bool // Check for exists
+            err = db.Raw("SELECT EXISTS(SELECT code FROM referrers WHERE code=?)", parts[1]).Find(&referrer).Error
+            if err != nil {
+                WarnAdmin(err)
+            }
+            if referrer {
+                err = db.Raw("UPDATE referrers SET users=users+1 WHERE code=?", parts[1]).Error
+                if err != nil {
+                    WarnAdmin(err)
+                }
+            }
+        }
+        return
     }
     
     switch update.Message.Text {
@@ -124,20 +141,6 @@ func handleMessage(update *tgbotapi.Update) {
         analytics.Bot(update.Message.Chat.ID, msg.Text, "Help")
         
     default: // Сообщение не является командой.
-        if ok, parts := strings.HasPrefix(update.Message.Text, "/start "), strings.Fields(update.Message.Text); ok && len(parts) == 2 {
-            var referrer bool // Check for exists
-            err = db.Raw("SELECT EXISTS(SELECT code FROM referrers WHERE code=?)", parts[1]).Find(&referrer).Error
-            if err != nil {
-                WarnAdmin(err)
-            }
-            if referrer {
-                err = db.Raw("UPDATE referrers SET users=users+1 WHERE code=?", parts[1]).Error
-                if err != nil {
-                    WarnAdmin(err)
-                }
-            }
-        }
-    
     
         userStep, err := getUserStep(update.Message.Chat.ID)
         if err != nil {
