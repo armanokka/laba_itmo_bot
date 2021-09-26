2bf8dc7a8ef529cf30400594585005c56bb79d9c
armanokka <armanokka@mail.ru>
2021-09-26 09:56:51 +0300

    add db configs

--- main.go
+++ main.go
@@ -63,10 +63,21 @@ func main() {
 
 	// Initializing PostgreSQL DB
 	var err error
-	db, err = gorm.Open(mysql.Open("f0568401_user:NlEbEgda@tcp(141.8.193.236:3306)/f0568401_user?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{SkipDefaultTransaction: true, PrepareStmt: false})
+	db, err = gorm.Open(mysql.Open("f0568401_user:NlEbEgda@tcp(141.8.193.236:3306)/f0568401_user?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{
+		SkipDefaultTransaction:                   true,
+	})
 	if err != nil {
 		panic(err)
 	}
+
+	sqlDb, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+	sqlDb.SetMaxOpenConns(25)
+	sqlDb.SetMaxIdleConns(10)
+	sqlDb.SetConnMaxLifetime(6 * time.Hour)
+
 	
 	analytics = dashbot.NewAPI(DashBotAPIKey, WarnErrorAdmin)
 	
