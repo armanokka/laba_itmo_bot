5a537330f96ec4b29c510178821bdad307e7bc6e
Arman <armanokka@mail.ru>
2022-11-26 14:31:34 +0300

    feat: remove lingvo temporarily

--- cmd/bot/message.go
+++ cmd/bot/message.go
@@ -7,7 +7,6 @@ import (
 	"github.com/armanokka/translobot/internal/tables"
 	"github.com/armanokka/translobot/pkg/errors"
 	"github.com/armanokka/translobot/pkg/helpers"
-	"github.com/armanokka/translobot/pkg/lingvo"
 	"github.com/armanokka/translobot/pkg/translate"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"go.uber.org/zap"
@@ -728,28 +727,26 @@ func (app *App) onMessage(ctx context.Context, message tgbotapi.Message) {
 	//	}
 	//	return g.Wait()
 	//})
-	if from == user.MyLang {
-		_, ok1 := lingvo.Lingvo[from]
-		_, ok2 := lingvo.Lingvo[to]
-		if ok1 && ok2 && len(text) < 50 && !strings.ContainsAny(text, " \r\n") {
-			g.Go(func() error {
-				ctx, _ := context.WithTimeout(ctx, time.Second*3)
-				l, err := lingvo.GetDictionary(ctx, from, to, strings.ToLower(text))
-				if err != nil {
-					if IsCtxError(err) {
-						return nil
-					}
-					log.Error("lingvo err", zap.Error(err))
-					return err
-				}
-				tr := strings.TrimSpace(writeLingvo(l))
-				if tr != "" {
-					trDict = tr + "\n❤️ @TransloBot"
-				}
-				return nil
-			})
-		}
-	}
+	//_, ok1 := lingvo.Lingvo[from]
+	//_, ok2 := lingvo.Lingvo[to]
+	//if ok1 && ok2 && len(text) < 50 && !strings.ContainsAny(text, " \r\n") {
+	//	g.Go(func() error {
+	//		ctx, _ := context.WithTimeout(ctx, time.Second*3)
+	//		l, err := lingvo.GetDictionary(ctx, from, to, strings.ToLower(text))
+	//		if err != nil {
+	//			if IsCtxError(err) {
+	//				return nil
+	//			}
+	//			log.Error("lingvo err", zap.Error(err))
+	//			return err
+	//		}
+	//		tr := strings.TrimSpace(writeLingvo(l))
+	//		if tr != "" {
+	//			trDict = tr + "\n❤️ @TransloBot"
+	//		}
+	//		return nil
+	//	})
+	//}
 
 	if err = g.Wait(); err != nil && !errors.Is(err, context.Canceled) {
 		log.Error("", zap.Error(err))
